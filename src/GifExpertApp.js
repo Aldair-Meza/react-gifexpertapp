4a6c63d8a1df95420c53914043e74929f295c77f
Aldair-Meza <amezasilgado@gmail.com>
2022-01-30 12:33:48 -0500

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+const GifExpertApp = props => {
+
+  //const categories = ['One Punch', 'Dragon Ball', 'Hellsing'];
+  const [categories, setCategories] = useState(['Hellsing']);
+
+  //const handleAdd = () => {
+    //setCategories([...categories, 'Kimetsu'])
+   // setCategories(cats =>[...cats,'Kimetsu']);
+
+ // }
+    return (
+        <>
+          <h2>GifExpertApp</h2>
+          <AddCategory setCategories={setCategories}/>         
+           <hr />  
+
+          
+
+          <ol>
+           {
+             categories.map(category =>
+                <GifGrid 
+                key={category}
+                category={ category}/>
+             )
+           }
+          </ol>
+
+        </>
+    )
+}
+
+GifExpertApp.propTypes = {
+
+}
+
+export default GifExpertApp
